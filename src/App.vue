e277a22181d63d685b43c5c67a992dd0b048cbd2
洪元晟 <>
2023-04-25 14:54:14 +0800

    course4 init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <el-container>
+    <el-header>
+      <el-tabs v-model="tabName" @tab-click="handleClick">
+        <el-tab-pane label="範例頁面" name="範例頁面"></el-tab-pane>
+      </el-tabs>
+    </el-header>
+    <el-container>
+      <el-main>
+        <Demo v-if="tabName == '範例頁面'"/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+// TODO: 教學 -- ref, reactive, onMounted
+import { onMounted, reactive, ref } from 'vue'
+import Demo from '@/components/Demo.vue'
+import type { TabsPaneContext } from 'element-plus'
+
+const tabName = ref('範例頁面')
+
+/// 已報名的列表
+const signUpList = ref<any>([])
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  /// 如果想偵測tab點擊事件可以加在這邊
+  console.log(tab, event)
+}
+
+function onSubmit(form: any) {
+  /// 接收到報名的表單，這邊不審核直接加入已報名列表
+  signUpList.value.push(form);
+}
+
+onMounted(() => {
+
+})
+</script>
+
+<style scoped>
+.app-root {
+  text-align: center;
+}
+
+.form-region {
+  margin-top: 40px;
+  display: inline-block;
+  width: 500px;
+}
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="demo">
+        <h1>Hellow 
+            <span class="name">{{ name }}</span>
+        </h1>
+        <div>
+            <el-input placeholder="請輸入" v-model="myinput" clearable class="my-input"></el-input>
+            <div>這裡是你輸入的內容：
+                <div class="show-input">{{ myinput }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script setup lang='ts'>
+import { ref, reactive, onMounted, watch } from 'vue'
+const name = ref('你好嗎？衷心感謝')
+const myinput = ref('')
+
+watch(() => myinput.value, (newValue, oldValue) => {
+    console.log('myinput: ', myinput.value);
+    // console.log('新值: ', newValue, ' 舊值: ', oldValue);
+})
+
+// onMounted(() => {
+//     console.log('name: ', name.value);
+// })
+</script>
+    
+<style>
+#demo {
+    text-align: center;
+}
+
+h1 {
+    font-size: 50px;
+    color:darkorange;
+    font-weight: 900;
+}
+
+.name {
+    color: darkcyan;
+}
+
+.my-input {
+    width: 500px !important;
+    margin-bottom: 10px;
+}
+
+.show-input {
+    font-size: 34px;
+    color: midnightblue;
+    font-weight: 500;
+}
+</style>
